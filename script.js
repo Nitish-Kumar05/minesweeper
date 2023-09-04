c6dc1fca4eaba97d46350400b3b3d7f419c61d45
Nitish-Kumar05 <mvnitishkumar@gmail.com>
2023-09-04 09:36:23 +0530

    populated numbers in remaining grid

--- script.js
+++ script.js
@@ -11,13 +11,54 @@ const createBoard = () => {
   const gameArray = emptyArray.concat(minesArray);
   const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
 
-  //   This loop will populate the mines and valids in grid
+  // This loop will populate the mines and valids in grid
   for (let i = 0; i < width * width; i++) {
     const square = document.createElement("div");
     square.setAttribute("id", i);
     square.classList.add(shuffledArray[i]);
     grid.appendChild(square);
     squares.push(square);
+  }
+
+  // In here the numbers will be populated to remaining grids
+  for (let i = 0; i < squares.length; i++) {
+    let total = 0;
+    const isLeftEdge = i % width === 0;
+    const isRightEdge = i % width === width - 1;
+
+    if (squares[i].classList.contains("valid")) {
+      if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains("mine"))
+        total++;
+      if (
+        i > 9 &&
+        !isRightEdge &&
+        squares[i + 1 - width].classList.contains("mine")
+      )
+        total++;
+      if (i > 10 && squares[i - width].classList.contains("mine")) total++;
+      if (
+        i > 11 &&
+        !isLeftEdge &&
+        squares[i - 1 - width].classList.contains("mine")
+      )
+        total++;
+      if (i < 98 && !isRightEdge && squares[i + 1].classList.contains("mine"))
+        total++;
+      if (
+        i < 90 &&
+        !isLeftEdge &&
+        squares[i - 1 + width].classList.contains("mine")
+      )
+        total++;
+      if (
+        i < 88 &&
+        !isRightEdge &&
+        squares[i + 1 + width].classList.contains("mine")
+      )
+        total++;
+      if (i < 89 && squares[i + width].classList.contains("mine")) total++;
+      squares[i].setAttribute("data", total);
+    }
   }
 };
 
