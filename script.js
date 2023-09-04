3c345b59fc2d2c661a951e505237f3962919f140
Nitish-Kumar05 <mvnitishkumar@gmail.com>
2023-09-04 09:12:59 +0530

    created a random array function

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const grid = document.querySelector(".grid");
 let width = 10;
+let landMines = 20;
 let squares = [];
 
 // To create board for the game
 const createBoard = () => {
+  // this will shuffle the land mines
+  const minesArray = Array(landMines).fill("mine");
+  const emptyArray = Array(width * width - landMines).fill("valid");
+  const gameArray = emptyArray.concat(minesArray);
+  const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
+
   for (let i = 0; i < width * width; i++) {
     const square = document.createElement("div");
     square.setAttribute("id", i);
